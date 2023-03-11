a54ed7507e8882188a35be37208e9f84ae252711
Dmitriy Belousov <d-belousov@hotmail.com>
2023-03-11 11:13:14 +0500

    Build: добавлен личный кабинет

---
+++ src/components/PersonalAcaunt.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from "@mui/material";
+
+function PersonalAcaunt () {
+  return (
+    <Container maxWidth="xl" >
+      container
+    </Container>
+  )
+}
+
+export default PersonalAcaunt
---
+++ src/constants/user.js
@@ -0,0 +1,19 @@
+const user={
+  name: 'Ivan',
+  secondName: 'Petrov',
+  company: 'ABC Constructor',
+  job: 'site namnager'
+}
+
+const projects={
+  build: {
+    name: 'build',
+    id: 1
+  },
+  work: {
+    name: 'work',
+    id: 2
+  }
+}
+
+export {user, projects}
