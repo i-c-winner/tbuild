57eb652fd8ac7a919f2e457e948c49a2459d9cf2
Dmitriy Belousov <d-belousov@hotmail.com>
2023-03-19 21:11:55 +0500

    Fix:

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,22 +2,22 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 function Row(props) {
-
+function getPath(id) {
+  return `/orders/${id}`
+}
   return (
     <tr>
       <React.Fragment>
         {Object.entries(props.row).map(cell => {
-          const path=`/orders/${props.row.id}`
-          console.log(path)
           if (cell[0] === 'total_price') {
             console.log(props.row.value)
-            return <td key={cell[0]}><Link to="path">{props.row.value * props.row.init_price}</Link></td>
+            return <td key={cell[0]}><Link to={getPath(props.row.id)}>{props.row.value * props.row.init_price}</Link></td>
           } else if (cell[0] === 'price') {
-            return <td key={cell[0]}><Link to="path">{props.row.ordered_value * props.row.init_price}</Link></td>
+            return <td key={cell[0]}><Link to={getPath(props.row.id)}>{props.row.ordered_value * props.row.init_price}</Link></td>
           } else if (cell[0] === 'last_price') {
-            return <td key={cell[0]}><Link to="path">{props.row.value * props.row.init_price}</Link></td>
+            return <td key={cell[0]}><Link to={getPath(props.row.id)}>{props.row.value * props.row.init_price}</Link></td>
           } else {
-            return <td key={cell[0]}><Link to="path">{cell[1]}</Link></td>
+            return <td key={cell[0]}><Link to={getPath(props.row.id)}>{cell[1]}</Link></td>
           }
         })}
       </React.Fragment>
